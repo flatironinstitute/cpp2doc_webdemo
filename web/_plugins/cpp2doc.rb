19bb7aa7c45f3f2e615cc0a9c4ba7dd47d35a345
Olivier Parcollet <olivier.parcollet@cea.fr>
2020-04-23 13:40:52 -0400

    WIP

--- web/_plugins/cpp2doc.rb
+++ web/_plugins/cpp2doc.rb
@@ -1,15 +1,52 @@
 module Jekyll
-  module MyFilter
+
+  module Reading
+  
+  # This function is executed before rendering the pages
+  # Cf Jekyll help, custom Generators
+  # It constructs
+  # briefs :  a hash table  qualified_name  --> brief documentation
+  # highlighted_types : a list of qualified name of type which are documented and 
+  # will be used to make link in highlighted code
+  # 
+  class Generator < Jekyll::Generator
+    def generate(site)
+      briefs = {}
+      classes = []
+      site.pages.each do |page|
+        if page['layout'] == 'function' or page['layout'] == 'class'
+          briefs[page['qualified_name']] = page['brief']
+        end 
+        if page['layout'] == 'class'
+          classes.append(page['qualified_name'])
+        end
+      end
+      site.data["allbriefs"] = briefs 
+      site.data["highlighted_types"] = classes
+      #puts "Computing briefs"
+      #puts briefs
+    end
+  end
+  end
+  
+  module CPP2DOC_Filter
    
-    def get_brief(qname, allbriefs)
-      allbriefs [qname]
+    # This filter associates the brief doc to a qualified name using the global table 
+    # The point is that the data is on *another* page.
+    # We must pass the table since a filter does not have the site variable 
+    # FIXME : Can this be fixed ? Can I not pass site_allbriefs ? 
+    def get_brief(qname, site_allbriefs)
+      site_allbriefs [qname]
     end
 
+    # This filter takes :
+    #  - source  : c++ code snipper
+    #  - highlighted_types : the global list of types from the site
+    #  - namespace_list : namespace where the page being rendered.
     def link_and_highlight(source, highlighted_types, namespace_list= [])
    
-      require 'rouge'
+      require 'rouge' # we use the highlighter
     
-     puts source 
       current_namespace = namespace_list.join('::')
       if not current_namespace.empty? then 
         current_namespace += '::'
@@ -54,37 +91,6 @@ module Jekyll
 
   end
   
-  module Reading
-  class Generator < Jekyll::Generator
-    def generate(site)
-
-      briefs = {}
-      classes = []
-      site.pages.each do |page|
-        if page['layout'] == 'function'  or page['layout'] == 'class'
-          briefs[page['qualified_name']] = page['brief']
-        end 
-        if page['layout'] == 'class'
-          classes.append(page['qualified_name'])
-        end
-      end
-      #briefs = site.pages.map{ |page| page.qualified_name
-      #reading = site.pages.detect {|page| page.name == 'K2.md'}
-      #readingKKK = site.pages.detect {|page| page.name == 'KKKK.md'}
-      #reading.data['injected'] = "INJECTED DATA: " + readingKKK.data["documentation"]['long']
-     
-      site.data["allbriefs"] = briefs 
-
-      # Just for the demo
-      site.data["highlighted_types"] = classes
-
-      #Dir.glob("_pages/doc/**/*.{md}")
-      puts "Computing briefs"
-      #puts briefs
-    end
-  end
 end
 
-end
-
-Liquid::Template.register_filter(Jekyll::MyFilter)
+Liquid::Template.register_filter(Jekyll::CPP2DOC_Filter)
