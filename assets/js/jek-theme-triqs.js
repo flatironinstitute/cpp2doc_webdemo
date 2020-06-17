d17e49eedf595feacb8f1d494934009f3039d432
lizlove <elizabethlovero@gmail.com>
2020-06-17 11:06:52 -0500

    Change xl breakpoint to 1441 and remove console log.

--- assets/js/jek-theme-triqs.js
+++ assets/js/jek-theme-triqs.js
@@ -51,8 +51,6 @@ function initSearch() {
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
       // Success!
-      var data = JSON.parse(request.responseText);
-      console.log('🗺️', data);
       {% if site.search_tokenizer_separator != nil %}
       lunr.tokenizer.separator = {{ site.search_tokenizer_separator }}
       {% else %}
